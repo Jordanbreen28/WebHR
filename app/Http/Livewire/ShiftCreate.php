6743bc66db323c81685981380abd9475155a1693
Jordanbreen28 <breen-j6@ulster.ac.uk>
2021-10-06 15:47:31 +0100

    Updates to creating shifts form

--- app/Http/Livewire/ShiftCreate.php
+++ app/Http/Livewire/ShiftCreate.php
@@ -4,7 +4,9 @@ namespace App\Http\Livewire;
 
 use Livewire\Component;
 use App\Models\User;
-use App\Models\ShiftTimes;
+use App\Models\Shifts;
+use Illuminate\Support\Facades\Auth;
+use App\Http\Controllers\ShiftController;
 use Carbon\Carbon;
 
 class ShiftCreate extends Component
@@ -15,6 +17,13 @@ class ShiftCreate extends Component
     public $start_time;
     public $end_time;
 
+    protected $rules = [
+        'employee' => 'required',
+        'shift_date' => 'required',
+        'start_time' => 'required',
+        'end_time' => 'required'
+    ];
+
     public function render()
     {
         return view('livewire.shift-create', [
@@ -22,30 +31,34 @@ class ShiftCreate extends Component
         ]);
     }
 
-    public function mount()
-    {
-        //$this->employees = $employees;//Pass employee array from controller to livewire component
-    }
-
     public function createShift()
     {
-        //$this->validate(); //add vailidation in form protected $rules = []
-        $employeeID = $this->employee;
-        $shift_date = $this->shift_date;
-        //parse time field into readable format for MySQL and MySQL
-        $start_time = $this->start_time;
-        $end_time = $this->end_time;
-        $start_time = Carbon::createFromTimestamp(strtotime($this->shift_date . $start_time . ":00"))->format('Y-m-d H:i:s');
-        $end_time = Carbon::createFromTimestamp(strtotime($this->shift_date . $end_time . ":00"))->format('Y-m-d H:i:s');
+                $this->validate(); //add vailidation in form protected $rules = []
+                
+                $employeeID = $this->employee;
+                $shift_date = $this->shift_date;
+                //parse time field into readable format for MySQL
+                $start_time = $this->start_time;
+                $end_time = $this->end_time;
+                $start_time = Carbon::createFromTimestamp(strtotime($this->shift_date . $start_time . ":00"))->format('Y-m-d H:i:s');
+                $end_time = Carbon::createFromTimestamp(strtotime($this->shift_date . $end_time . ":00"))->format('Y-m-d H:i:s');
 
-        ShiftTimes::create([
-            'user_id' => $employeeID,
-            'shift_date' => $shift_date,
-            'shift_start' => $start_time,
-            'shift_end'=> $end_time,
-            
+                $employee_name = User::all()->where('id', $employeeID)->pluck('name')->first();
 
-        ]);
-        return redirect('/shift-create/{id}');
-    }
+                Shifts::create([
+                    'user_id' => $employeeID,
+                    'created_by' => Auth::id(),
+                    'shift_date' => $shift_date,
+                    'shift_start' => $start_time,
+                    'shift_end'=> $end_time,
+                ]);
+
+                session()->flash('message', 'Shift successfully created and email sent to '.$employee_name);
+                //clear session values
+                $this->employee = '';
+                $this->shift_date = '';
+                $this->start_time = '';
+                $this->end_time = '';
+}
+
 }
--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Auth;
 use App\Http\Controllers;
 use App\Http\Livewire\ShiftCreate;
+use App\Http\Livewire\ShiftTime;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -29,7 +30,9 @@ Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
 
 //return messages view with Auth user messages
 Route::get('/messages', 'App\Http\Controllers\MessageController@retrieveUserMessage')->name('messages');
-//send message
+//Open specific message
+Route::get('/messages/{message_id}', 'App\Http\Controllers\MessageController@openMessage')->name('open-message');
+//Create message
 Route::post('/messages', 'App\Http\Controllers\MessageController@sendMessage')->name('outbox');
 
 /*Shift Routes
@@ -37,11 +40,10 @@ Route::post('/messages', 'App\Http\Controllers\MessageController@sendMessage')->
 */
 
 //Render livewire component - > create shift using livewire aync call, no need for POST
-Route::get('/shift-create', ShiftCreate::class)->name('shift-create');
-Route::get('/shift-create/{id}', ShiftCreate::class)->name('shift-create/');
-//Retrieve clock in view and time
-Route::get('/shift-times', 'App\Http\Controllers\ShiftTimeController@index')->name('shift-times');
+Route::get('/shifts', ShiftCreate::class)->name('shifts');
+
+Route::get('/view-shifts', 'App\Http\Controllers\ShiftController@index')->name('current-shift');
+
+Route::get('/shifts/{id}', 'App\Http\Controllers\ShiftController@getShift')->name('current-shift');
 //Record Clock in Shift Times 
-Route::put('/shift-times/{id}', 'App\Http\Controllers\ShiftTimeController@recordClockIn')->name('clock-in');
-//Record Clock out Shift time
-Route::put('/shift-times/{id}', 'App\Http\Controllers\ShiftTimeController@recordClockOut')->name('clock-out');
+Route::put('/shifts/{id}', 'App\Http\Controllers\ShiftTimeController@recordTime')->name('shift');
